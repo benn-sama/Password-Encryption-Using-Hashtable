fc7e7b1c85e96d25b0ddfc1e6650217e6661bc23
benn-sama <green.l.benjamin04@gmail.com>
2024-09-11 14:51:29 -0500

    fixed a few bugs and changed test cases

--- test/test-cases.cpp
+++ test/test-cases.cpp
@@ -3,16 +3,31 @@
 #include <cassert>
 #include <string>
 #include <exception>
+#include <vector>
 
 void test_random_password() {
   RandomPassword passwordGenerator;
-  std::string password = passwordGenerator.randomPassword();
+  std::vector<std::string> stringVector;
+  std::string password = "";
 
-  if (password.size() != 9) {
-    throw std::runtime_error("password size is not 9, password size is " + std::to_string(password.size()));
+  // tests password values 
+  std::cout << "Testing password values in an array..." << std::endl;
+  for (int i = 0; i < 5; ++i) {
+    password = passwordGenerator.randomPassword();
+    stringVector.push_back(password);
+
+    if (stringVector.at(i).size() != 9) {
+      throw std::runtime_error("Password " + std::to_string(i) + " is not of size!\n" 
+                             + "Password: " + stringVector.at(i));
+    }
+
+    std::cout << "Password " << i + 1 << ": " << stringVector.at(i) << std::endl;
   }
 
+  std::cout << std::endl << "Password values are sufficent!" << std::endl << std::endl;
+
   std::cout << "----------test_random_password passed----------" << std::endl;
+
 }
 
 int main() {
