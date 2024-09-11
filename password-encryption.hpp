6e7cfaac0e3ab8c610275202e5cc51b2a518c649
bgreen <155407150+benn-sama@users.noreply.github.com>
2024-09-11 00:52:58 -0500

    Add files via upload

---
+++ password-encryption.hpp
@@ -0,0 +1,11 @@
+#ifndef PASSWORD_ENCRYPTION_HPP
+#define PASSWORD_ENCRYPTION_HPP
+
+#include <string>
+
+class PasswordEncrypt {
+  private:
+    std::string password;
+};
+
+#endif
---
+++ random-password.hpp
@@ -0,0 +1,43 @@
+#ifndef RANDOM_PASSWORD_HPP
+#define RANDOM_PASSWORD_HPP
+
+#include <random>
+#include <string>
+
+class RandomPassword {
+  public:
+    // default constructor
+    RandomPassword() {}
+
+    // returns random password using Mersenne Twister random number generator
+    std::string randomPassword() {
+      std::string newPassword = "";
+      int addingIntoChar = 0;
+
+      // random generator declaration
+      std::random_device random;
+      std::mt19937 gen(random());
+      std::uniform_int_distribution<> dis(0, 93);
+
+      // adds a char into newPassword 9x
+      for (int i = 0; i < 9; ++i) {
+        char passwordIndex = '!';
+        addingIntoChar = dis(gen);
+
+        // checks if passwords are consecutive. 
+        // if consecutive, randomize password index until otherwise
+        if (i != 0) {
+          while ((addingIntoChar == newPassword[i - 1] - 1) || (addingIntoChar == newPassword[i + 1] + 1)) {
+            addingIntoChar = dis(gen);
+          }
+        }
+
+        passwordIndex += addingIntoChar;
+        newPassword.append(1, passwordIndex);
+      }
+
+      return newPassword;
+    }
+};
+
+#endif
